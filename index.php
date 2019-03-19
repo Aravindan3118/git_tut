8bb7c2bdb2555d1919cc6a3f853cfeb7da409a5d
Aravindan3118 <aravindannichu007@gmail.com>
2019-03-19 15:12:30 +0530

    Added Image to home page

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ echo "second welcome message from another";
   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
   dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
    sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+   <img src="https://cdn.pixabay.com/photo/2017/05/09/21/49/gecko-2299365_960_720.jpg" alt="">
 </p>
 <?php
 include "footer.php";
