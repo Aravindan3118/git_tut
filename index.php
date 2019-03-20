1ce8a2866bed25d5026bf3eee12bae5a70c7d18d
Aravindan3118 <aravindannichu007@gmail.com>
2019-03-20 16:38:51 +0530

    Last three image remove album feature

--- index.php
+++ index.php
@@ -18,15 +18,6 @@ echo "second welcome message from another";
 	<div class="col-md-4">
 		<img style="width: 100%;" src="https://cdn.pixabay.com/photo/2017/05/09/21/49/gecko-2299365_960_720.jpg" alt="">
 	</div>
-	<div class="col-md-4">
-		<img style="width: 100%;" src="https://cdn.pixabay.com/photo/2017/05/09/21/49/gecko-2299365_960_720.jpg" alt="">
-	</div>
-	<div class="col-md-4">
-		<img style="width: 100%;" src="https://cdn.pixabay.com/photo/2017/05/09/21/49/gecko-2299365_960_720.jpg" alt="">
-	</div>
-	<div class="col-md-4">
-		<img style="width: 100%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLb5mOUtzV0ObqBVuAURSvPAsC27148aFdKGc6e6Z_Z78vmMWf" alt="">
-	</div>
    
 </p>
 <?php
